73fe5ab66b3e2436ee2a18c0b56cc1c910e5f15b
Matheus <matheus.lima@ibbx.tech>
2023-09-18 09:38:32 -0300

    att Name styles -> style, + chart class

--- main.js
+++ main.js
@@ -1,12 +1,71 @@
-const getOptionChart1 = () => {};
-const getOptionChart2 = () => {};
+const getOptionChart1 = () => {
+  return {
+    xAxis: {
+      type: "category",
+      data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+    },
+    yAxis: {
+      type: "value",
+    },
+    series: [
+      {
+        data: [120, 200, 150, 80, 70, 110, 130],
+        type: "bar",
+        showBackground: true,
+        backgroundStyle: {
+          color: "rgba(180, 180, 180, 0.2)",
+        },
+      },
+    ],
+  };
+};
+const getOptionChart2 = () => {
+  return {
+    title: {
+      text: "World Population",
+    },
+    tooltip: {
+      trigger: "axis",
+      axisPointer: {
+        type: "shadow",
+      },
+    },
+    legend: {},
+    grid: {
+      left: "3%",
+      right: "4%",
+      bottom: "3%",
+      containLabel: true,
+    },
+    xAxis: {
+      type: "value",
+      boundaryGap: [0, 0.01],
+    },
+    yAxis: {
+      type: "category",
+      data: ["Brazil", "Indonesia", "USA", "India", "China", "World"],
+    },
+    series: [
+      {
+        name: "2011",
+        type: "bar",
+        data: [18203, 23489, 29034, 104970, 131744, 630230],
+      },
+      {
+        name: "2012",
+        type: "bar",
+        data: [19325, 23438, 31000, 121594, 134141, 681807],
+      },
+    ],
+  };
+};
 
 const initCharts = () => {
   const chart1 = echarts.init(document.getElementById("chart1"));
   const chart2 = echarts.init(document.getElementById("chart2"));
 
   chart1.setOption(getOptionChart1());
-  chart1.setOption(getOptionChart2());
+  chart2.setOption(getOptionChart2());
 };
 
 window.addEventListener("load", () => {
