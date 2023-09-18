5d303e6a3d1416b265f062523fb4838f416b2990
Matheus <matheus.lima@ibbx.tech>
2023-09-18 12:40:03 -0300

    Atualizações

--- main.js
+++ main.js
@@ -1,41 +1,91 @@
+class Coleta {
+  constructor() {
+    this.id = 1;
+    this.arrayColetas = [];
+  }
+  salvar() {
+    let coleta = this.lerDados();
+
+    if (this.validaCampos(coleta)) {
+      this.adicionar(coleta);
+    }
+
+    this.listaTabela();
+    this.cancelar();
+  }
+
+  listaTabela() {
+    let tbody = document.getElementById("tbody");
+    tbody.innerText = "";
+
+    for (let i = 0; i < this.arrayColetas.length; i++) {
+      let tr = tbody.insertRow();
+
+      let td_id = tr.insertCell();
+      let td_name = tr.insertCell();
+      let td_edit = tr.insertCell();
+
+      td_id.innerText = this.arrayColetas[i].nomeId;
+      td_name.innerText = this.arrayColetas[i].nome;
+
+      let imgEdit = document.createElement("img");
+      imgEdit.src = "./img/Edit.png";
+
+      let imgDelete = document.createElement("img");
+      imgDelete.src = "/img/Delete.jpg";
+
+      td_edit.appendChild(imgEdit);
+      td_edit.appendChild(imgDelete);
+    }
+  }
+
+  adicionar(coleta) {
+    this.arrayColetas.push(coleta);
+    this.id++;
+  }
+
+  lerDados() {
+    let coleta = {};
+
+    coleta.id = this.id;
+    coleta.nomeId = document.getElementById("valor").value;
+    coleta.nome = document.getElementById("name").value;
+
+    return coleta;
+  }
+
+  validaCampos(coleta) {
+    let msg = "";
+
+    if (coleta.nomeId == "") {
+      msg += "Informe O id do produto \n";
+    }
+    if (coleta.nome == "") {
+      msg += "Informe O nome do produto \n";
+    }
+    if (msg != "") {
+      alert(msg);
+      return false;
+    }
+    return true;
+  }
+
+  cancelar() {
+    document.getElementById("valor").value = "";
+    document.getElementById("name").value = "";
+  }
+}
+
+var coleta = new Coleta();
+
 const getOptionChart1 = () => {
   return {
-    // Para que Mostre o valor em tela
     tooltip: {
       show: true,
-      trigger: "axis",
-      triggerOn: "mousemove|click",
-    },
-    // Barra De Filtro
-    dataZoom: {
-      show: true,
-    },
-    xAxis: {
-      type: "category",
-      data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
-    },
-    yAxis: {
-      type: "value",
-    },
-    series: [
-      {
-        data: [120, 200, 150, 80, 70, 110, 130],
-        type: "bar",
-        showBackground: true,
-        backgroundStyle: {
-          color: "rgba(180, 180, 180, 0.2)",
-        },
-      },
-    ],
-  };
-};
-const getOptionChart2 = () => {
-  return {
-    tooltip: {
       trigger: "axis",
     },
     legend: {
-      data: ["Email", "Union Ads", "Video Ads", "Direct", "Search Engine"],
+      data: ["Coletas"],
     },
     grid: {
       left: "3%",
@@ -43,49 +93,24 @@ const getOptionChart2 = () => {
       bottom: "3%",
       containLabel: true,
     },
-    toolbox: {
-      feature: {
-        saveAsImage: {},
+    xAxis: [
+      {
+        type: "category",
+        boundaryGap: false,
+        data: ["Seg", "Ter", "Qua", "Qui", "Sex", "Sab", "Dom"],
       },
-    },
-    xAxis: {
-      type: "category",
-      boundaryGap: false,
-      data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
-    },
-    yAxis: {
-      type: "value",
-    },
+    ],
+    yAxis: [
+      {
+        type: "value",
+      },
+    ],
     series: [
       {
-        name: "Email",
+        name: "Coletas",
         type: "line",
         stack: "Total",
         data: [120, 132, 101, 134, 90, 230, 210],
-      },
-      {
-        name: "Union Ads",
-        type: "line",
-        stack: "Total",
-        data: [220, 182, 191, 234, 290, 330, 310],
-      },
-      {
-        name: "Video Ads",
-        type: "line",
-        stack: "Total",
-        data: [150, 232, 201, 154, 190, 330, 410],
-      },
-      {
-        name: "Direct",
-        type: "line",
-        stack: "Total",
-        data: [320, 332, 301, 334, 390, 330, 320],
-      },
-      {
-        name: "Search Engine",
-        type: "line",
-        stack: "Total",
-        data: [820, 932, 901, 934, 1290, 1330, 1320],
       },
     ],
   };
@@ -93,10 +118,8 @@ const getOptionChart2 = () => {
 
 const initCharts = () => {
   const chart1 = echarts.init(document.getElementById("chart1"));
-  const chart2 = echarts.init(document.getElementById("chart2"));
 
   chart1.setOption(getOptionChart1());
-  chart2.setOption(getOptionChart2());
 };
 
 window.addEventListener("load", () => {
